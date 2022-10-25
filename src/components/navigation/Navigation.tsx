6a73f5dd2c90621cc5405a17026ecdfab62ca244
Tricia Amheiser <tricia@taarts.net>
2022-10-25 15:53:34 -0400

    created navigation.css in the hope of using it within navigation component. NAA said I'm punching up again...

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,51 +1,58 @@
 import React from 'react'
+// import navigation from '.src/components/navigation/navigation.css'
 
 export function Navigation() {
   return (
     <div>
-      <nav>
-        <div className="left-hand-side">
-          <ul>
-            <li>
-              <a href="#">
-                <img
-                  id="git"
-                  src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-                  width="45"
-                  height="45"
-                  alt="GithubLogo"
-                />
-              </a>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <a href="#">Octodex</a>
-            </li>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="https://octodex.github.com/faq/">FAQ</a>
-            </li>
-          </ul>
+      <header>
+        <div className="container">
+          <nav>
+            <div className="left-hand-side">
+              <ul>
+                <li>
+                  <a href="#">
+                    <img
+                      id="git"
+                      src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                      width="45"
+                      height="45"
+                      alt="GithubLogo"
+                    />
+                  </a>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <a href="#">Octodex</a>
+                </li>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="https://octodex.github.com/faq/">FAQ</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <nav>
+            <ul>
+              <li id="twitter">
+                <a href="https://twitter.com/githubdesign">
+                  <span>Follow us on Twitter</span>
+                  <img src="/src/twitter.png" height="30" width="30" />
+                </a>
+              </li>
+              <li id="back">
+                <a href="https://github.com/">
+                  <span>Back to GitHub.com</span> <span id="alt">Back</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
-      </nav>
-      <nav>
-        <ul>
-          <li id="twitter">
-            <a href="https://twitter.com/githubdesign">
-              <span>Follow us on Twitter</span>
-              <img src="/src/twitter.png" height="30" width="30" />
-            </a>
-          </li>
-          <li id="back">
-            <a href="https://github.com/">
-              <span>Back to GitHub.com</span> <span id="alt">Back</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+      </header>
     </div>
   )
 }
+
+// Path: src/components/octocat.tsx
